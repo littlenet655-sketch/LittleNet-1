0f210053c29df234ed92ef1031c2a6a8e59bce28
Pragna G Shenoy <pragnashenoy897@gmail.com>
2026-09-06 00:17:48 +0530

    build(modal): optimize build ignore rules and add deploy_modal_ai tool

---
+++ tools/deploy_modal_ai.py
@@ -0,0 +1,14 @@
+import sys, os, subprocess
+
+sys.stdout.reconfigure(encoding='utf-8')
+sys.stderr.reconfigure(encoding='utf-8')
+
+env = os.environ.copy()
+env['PYTHONIOENCODING'] = 'utf-8'
+
+print("Deploying modal_ai.py with UTF-8 encoding...")
+res = subprocess.run([
+    sys.executable, "-m", "modal", "deploy", "modal_ai.py"
+], env=env)
+
+sys.exit(res.returncode)
